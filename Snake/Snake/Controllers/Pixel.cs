1370add0ad5b4c9552aeb864b911cc4ed59f3675
Jesus <jjorgeserran@gmail.com>
2013-04-12 20:32:12 +0200

    Move to Views directory

---
+++ Snake/Snake/Controllers/Pixel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake.Controllers
+{
+    public class Pixel
+    {
+        private int x;
+        private int y;
+
+        public int getX()
+        {
+            return this.x;
+        }
+    }
+}
